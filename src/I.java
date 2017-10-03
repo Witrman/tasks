f27a4c54c0695c3d2c6d48f75938feeed8e5c60f
Witrman <32022781+Witrman@users.noreply.github.com>
2017-10-03 21:22:18 +0400

    Initial commit

---
+++ src/I.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class I {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] str =sc.nextLine().toCharArray();
+        int sum = str[0]-48 + str[1]-48 + str[2]-48;
+        System.out.println(sum);
+        
+    }
+}
---
+++ src/J.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class J {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum =sc.nextInt();
+        sum /=2;
+        sum*=2;
+        sum+=2;
+        System.out.println(sum);
+    }
+}
