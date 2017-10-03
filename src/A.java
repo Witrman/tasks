1bef741ef81577f6dfdfb048aca3a95f8b72d299
Witrman <32022781+Witrman@users.noreply.github.com>
2017-10-03 22:55:02 +0400

    add

--- src/A.java
+++ src/A.java
@@ -2,11 +2,19 @@ import java.util.Scanner;
 
 public class A {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    /*    Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         double rez = (a*a)+(b*b);
         System.out.println(Math.sqrt(rez));
+ бля я делал не то....*/
+        int a = 13/5; int b = 13%5;
+     //   int c = 13.0/5; double на int
+        double d = 13/5; double e = 13%5;
+        double f = 13.0/5; double g = 13/5 + 2/5;
+        double h = 13.0/5 + 2.0/5;
+        //  int	i = 13.0/5 + 2.0/5; double на int
+        System.out.println(a+"   "+b+"   "+d+"   "+e+"   "+f+"   "+g+"   "+h);
 
     }
 }
---
+++ src/B_C_D.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class B_C_D {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+int x = sc.nextInt();
+     int y= x*x;
+       y = y*y;
+        System.out.println("степень 4="+y);
+        y= x*x;
+        y = y*y*y;
+        System.out.println("степень 6="+y);
+        y= x*x;
+        y = y*y*y*x;
+        System.out.println("степень 7="+y);
+        y= x*x;
+        y = y*y*y*x;
+        y = y*y*y;
+        System.out.println("степень 21="+y);
+    }
+}
