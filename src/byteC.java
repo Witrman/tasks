9973c36f33bb0ba10703944b4e3224f2381c437c
Witrman <32022781+Witrman@users.noreply.github.com>
2017-10-05 00:46:11 +0400

    nw

--- src/byteC.java
+++ src/byteC.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class byte1C {
+public class byteC {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             int a = sc.nextInt();
