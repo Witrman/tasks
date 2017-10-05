3c66dcd0c29a9b4ce0f43c46124b97e6faac74dd
Witrman <32022781+Witrman@users.noreply.github.com>
2017-10-05 20:39:58 +0400

    nw

--- src/File.java
+++ src/File.java
@@ -1,2 +1,10 @@
 public class File {
+    public static void main(String[] args) {
+        System.out.println("Con");
+        for (int i= 1; i<4;i++)
+        {
+            System.out.println(i+i*i+i*i*i*i*i+i/2);
+
+        }
+    }
 }
